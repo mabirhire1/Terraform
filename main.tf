aa3012bc49d4218f038e82a46d89a1d4d49f312a
Mercy Abirhire <mabirhire1@gmail.com>
2024-08-11 02:48:00 +0100

    updated the main.tf file

--- main.tf
+++ main.tf
@@ -19,6 +19,11 @@ locals {
   local_ip = chomp(data.http.myip.response_body)
 }
 
+resource "aws_key_pair" "angel" {
+   key_name   = "angel"
+   public_key = file("~/.ssh/angel.pub")
+}
+
 module "vpc" {
   source     = "./modules/vpc"
 
@@ -27,6 +32,14 @@ module "vpc" {
   private_subnet_cidr = "10.0.2.0/24"
 }
 
+module "ec2_instances" {
+  source                    = "./modules/ec2_instances"
+  public_subnet_id          = var.public_subnet_id
+  private_subnet_id         = var.private_subnet_id
+  public_security_group_id  = var.security_groups.public_sg_id
+  private_security_group_id = var.security_groups.private_sg_id
+  public_key_path           = var.public_key_path
+}
 
 
 
